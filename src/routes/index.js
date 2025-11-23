ebef564cbedd7170c7cbf1e852b83e7a03017ec1
Dorozhko Alexandr <darklin314@gmail.com>
2025-11-23 16:16:14 +0300

    fix: Исправлен роутинг

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,16 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import Main_Page from '../components/Pages/Main_Page.vue'
 import Post_Page from '../components/Pages/Post_Page.vue'
 
 const routes = [
-    {path: '/', name: 'mainPage', component: Main_Page},
-    {path: '/post/:id', name: 'postPage', component: Post_Page, props: true},
-
+  { path: '/', name: 'mainPage', component: Main_Page },
+  { path: '/post/:id', name: 'postPage', component: Post_Page, props: true },
 ]
 
-const router = createRouter ({
-    history: createWebHistory(),
-    routes,
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
 })
 
 export default router
